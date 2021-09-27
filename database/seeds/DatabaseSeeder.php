4978e47b34a6f1a1417e59f3d461184203525e55
Roberto Fernandes <roberto.fernandes@marandu.com.ar>
2021-09-27 07:26:33 -0300

    se limpia el codigo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Articulo;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ArticuloController;
 use Illuminate\Support\Facades\Route;
 
 /*
